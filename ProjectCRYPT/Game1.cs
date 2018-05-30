f6b99f0ee835fbe77fb2d49fad0db2a688fd7b9b
Tom Collyer <37063689+para-D0X@users.noreply.github.com>
2018-05-30 20:20:29 +1000

    GitHub all working now
    This should be working how it should now

---
+++ ProjectCRYPT/Game1.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+using MonoGame.Extended.ViewportAdapters;
+
+
+namespace ProjectCRYPT
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Player player = null;
+
+        Camera2D camera = null;
+        TiledMap map = null;
+        TiledMapRenderer mapRenderer = null;
+        TiledMapTileLayer collisionLayer;
+
+        public static int tile = 16;
+        public static float meter = tile;
+        public static Vector2 maxVelocity = new Vector2(meter * 10, meter * 10);
+        public static float xAcceleration = maxVelocity.X * 4;
+        public static float yAcceleration = maxVelocity.Y * 4;
+        public static float xFriction = maxVelocity.X * 6;
+        public static float yFriction = maxVelocity.Y * 6;
+
+        public int ScreenWidth
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Width;
+            }
+        }
+
+        public int ScreenHeight
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Height;
+            }
+        }
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+
+        }
+
+        protected override void Initialize()
+        {
+
+            player = new Player(this);
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player.Load(Content);
+
+            BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice,  
+                ScreenWidth, ScreenHeight);
+
+            camera = new Camera2D(viewportAdapter);
+            camera.Position = new Vector2(-100, -100);
+
+            map = Content.Load<TiledMap>("crypttestmap");
+            mapRenderer = new TiledMapRenderer(GraphicsDevice);
+
+            foreach (TiledMapTileLayer layer in map.TileLayers)
+            {
+                if (layer.Name == "Collisions") ;
+                collisionLayer = layer;
+            }
+
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            player.Update(deltaTime);
+
+            //camera.Move(new Vector2(0, -50) * deltaTime);
+            camera.Zoom = 1f;
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            var viewMatrix = camera.GetViewMatrix();
+            var projectionMatrix = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0f, -1f);
+
+            spriteBatch.Begin(transformMatrix : viewMatrix);
+
+            mapRenderer.Draw(map, ref viewMatrix, ref projectionMatrix);
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public int PixelToTile(float pixelCoord)
+        {
+            return (int)Math.Floor(pixelCoord / tile);
+        }
+
+        public int TileToPixel(int tileCoord)
+        {
+            return tile * tileCoord;
+        }
+
+        public int CellAtPixelCoord(Vector2 pixelCoords)
+        {
+            if (pixelCoords.X < 0 || pixelCoords.X > map.WidthInPixels || pixelCoords.Y < 0)
+                return 1;
+
+            if (pixelCoords.Y > map.HeightInPixels)
+                return 0;
+            return CellAtTileCoord(PixelToTile(pixelCoords.X), PixelToTile(pixelCoords.Y));
+
+        }
+
+        public int CellAtTileCoord(int tx, int ty)
+        {
+            if (tx < 0 || tx >= map.Width || ty < 0)
+                return 1;
+
+            if (ty >= map.Height)
+                return 0;
+
+            TiledMapTile? tile;
+            collisionLayer.TryGetTile(tx, ty, out tile);
+            return tile.Value.GlobalIdentifier;
+
+        }
+    }
+}
---
+++ ProjectCRYPT/Player.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectCRYPT
+{
+    class Player
+    {
+        Sprite playerSprite = new Sprite();
+
+        Game1 game = null;
+
+        Vector2 velocity = Vector2.Zero;
+        Vector2 position = Vector2.Zero;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return playerSprite.position;
+            }
+        }
+
+        public Player(Game1 game)
+        {
+            this.game = game;
+            velocity = Vector2.Zero;
+            position = Vector2.Zero;
+        }
+
+        public void Load(ContentManager content)
+        {
+            playerSprite.Load(content, "tempPlayer");
+        }
+
+        public void Update(float deltaTime)
+        {
+            playerSprite.Update(deltaTime);
+            UpdateInput(deltaTime);
+        }
+
+        private void UpdateInput(float deltaTime)
+        {
+            bool wasMovingLeft = velocity.X < 0;
+            bool wasMovingRight = velocity.X > 0;
+            bool wasMovingUp = velocity.Y < 0;
+            bool wasMovingDown = velocity.Y > 0;
+
+            Vector2 acceleration = new Vector2(0, 0);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A) == true)
+            {
+                acceleration.X -= Game1.xAcceleration;
+            }
+            else if (wasMovingLeft == true)
+            {
+                acceleration.X += Game1.xFriction;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D) == true)
+            {
+                acceleration.X += Game1.xAcceleration;
+            }
+            else if (wasMovingRight == true)
+            {
+                acceleration.X -= Game1.xFriction;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) == true)
+            {
+                acceleration.Y -= Game1.yAcceleration;
+            }
+            else if (wasMovingUp == true)
+            {
+                acceleration.Y += Game1.yFriction;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S) == true)
+            {
+                acceleration.Y += Game1.yAcceleration;
+            }
+            else if (wasMovingDown == true)
+            {
+                acceleration.Y -= Game1.yFriction;
+            }
+
+            velocity += acceleration * deltaTime;
+
+            velocity.X = MathHelper.Clamp(velocity.X, -Game1.maxVelocity.X, Game1.maxVelocity.X);
+            velocity.Y = MathHelper.Clamp(velocity.Y, -Game1.maxVelocity.Y, Game1.maxVelocity.Y);
+
+            playerSprite.position += velocity * deltaTime;
+
+            if ((wasMovingLeft && (velocity.X > 0)) || (wasMovingRight && (velocity.X < 0)))
+            {
+                velocity.X = 0;
+            }
+
+            if ((wasMovingDown && (velocity.Y < 0)) || (wasMovingUp && (velocity.Y > 0)))
+            {
+                velocity.Y = 0;
+            }
+
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            playerSprite.Draw(spriteBatch);
+        }
+
+
+
+
+
+
+
+
+    }
+}
