c5a30633b538370aa4f46fb9ac592aad4e53d29b
titanslayer11 <37866425+titanslayer11@users.noreply.github.com>
2018-07-07 10:39:12 +1000

    creating the turret class and deleting code that wasn't doing anything

--- ProjectCRYPT/Turret.cs
+++ ProjectCRYPT/Turret.cs
@@ -11,125 +11,113 @@ using System.Threading.Tasks;
 
 namespace ProjectCRYPT
 {
-    class Zombie 
+    class Turret
     {
         GraphicsDeviceManager graphics;
-        public Sprite zombieSprite = new Sprite();
-        
+        public Sprite turretSprite = new Sprite();
 
         Game1 game = null;
 
-
         float distance = 75;
-
-        Vector2 position = Vector2.Zero;
-        Vector2 velocity = Vector2.Zero;
 
         public Player GetPlayer { get; set; }
         public Fireball GetFireball { get; set; }
         public bool isAlive;
-        public float zombieRotation = 0f;
-        float zombieSpeed = 25f;
-        
-        Texture2D zombie;
+        Vector2 position = Vector2.Zero;
+        Vector2 velocity = Vector2.Zero;
+        public float turretRotation = 40f;
 
-        AnimatedTexture zombieAnimation = new AnimatedTexture(Vector2.Zero, 0, 1, 1);
+        Texture2D turret;
 
-        public Rectangle Bounds
-        {
-            get { return zombieSprite.Bounds; }
-        }
+        AnimatedTexture turretAnimation = new AnimatedTexture(Vector2.Zero, 0, 1, 1);
 
         public Vector2 Position
         {
             get
             {
-                return zombieSprite.position;
+                return turretSprite.position;
             }
             set
             {
-                zombieSprite.position = value;
+                turretSprite.position = value;
             }
         }
 
         float RotateTo(Vector2 pointTo)
         {
             float rot = 0;
-            
+
             Vector2 direction = position - pointTo;
             direction.Normalize();
 
-            rot = (float)Math.Atan2((double)direction.Y,(double)direction.X);
+            rot = (float)Math.Atan2((double)direction.Y, (double)direction.X);
 
             rot += MathHelper.ToRadians(180);
 
             return rot;
         }
 
-        public Zombie(Game1 game)
+        public Turret(Game1 game)
         {
             this.game = game;
-            Position = new Vector2(100,0);
-            zombieRotation = 0;
+            Position = new Vector2(100, 0);
+            turretRotation = 40;
         }
-        
+
         public void Load(ContentManager content)
         {
-            zombie = content.Load<Texture2D>("zombie");
+            turret = content.Load<Texture2D>("turret");
 
-            zombieAnimation.Load(content, "zombie", 1, 1);
+            turretAnimation.Load(content, "turret", 1, 1);
 
-            zombieAnimation.Origin = new Vector2(zombie.Width / 2, zombie.Height / 2);
+            turretAnimation.Origin = new Vector2(turret.Width / 2, turret.Height / 2);
 
-            zombieSprite.Add(zombieAnimation, zombie.Width / 2, zombie.Height / 2);
-            zombieSprite.Pause();
+            turretSprite.Add(turretAnimation, turret.Width / 2, turret.Height / 2);
+            turretSprite.Pause();
 
-            
+
 
         }
 
         public void Update(float deltaTime)
         {
-            zombieSprite.Update(deltaTime);
+            turretSprite.Update(deltaTime);
             UpdateInput(deltaTime);
-            zombieRotation = RotateTo(GetPlayer.Position);
-            zombieAnimation.Rotation = zombieRotation;
-            position = zombieSprite.position;
+            turretRotation = RotateTo(GetPlayer.Position);
+            turretAnimation.Rotation = turretRotation;
+            position = turretSprite.position;
         }
 
         private void UpdateInput(float deltaTime)
         {
-            int tx = game.PixelToTile(zombieSprite.position.X);
-            int ty = game.PixelToTile(zombieSprite.position.Y);
+            /*int tx = game.PixelToTile(turretSprite.position.X);
+            int ty = game.PixelToTile(turretSprite.position.Y);
 
-            bool nx = (zombieSprite.position.X) % Game1.tile != 0;
+            bool nx = (turretSprite.position.X) % Game1.tile != 0;
 
-            bool ny = (zombieSprite.position.Y) % Game1.tile != 0;
+            bool ny = (turretSprite.position.Y) % Game1.tile != 0;
 
             bool cell = game.CellAtTileCoord(tx, ty) != 0;
             bool cellright = game.CellAtTileCoord(tx + 1, ty) != 0;
             bool celldown = game.CellAtTileCoord(tx, ty + 1) != 0;
             bool celldiag = game.CellAtTileCoord(tx + 1, ty + 1) != 0;
 
-            Vector2.Distance(GetPlayer.Position, zombieSprite.position);
+            Vector2.Distance(GetPlayer.Position, turretSprite.position);
 
-            if(Vector2.Distance(GetPlayer.Position, zombieSprite.position) <= distance)
+            if (Vector2.Distance(GetPlayer.Position, turretSprite.position) <= distance)
             {
                 Vector2 direction;
 
                 direction = GetPlayer.Position - Position;
                 direction.Normalize();
-
-                velocity = direction * (Game1.maxVelocity * deltaTime);
-
-                zombieSprite.position += velocity * zombieSpeed * deltaTime;
+                                                
             }
-                      
+
             if (this.velocity.Y > 0)
             {
                 if ((celldown && !cell) || (celldiag && !cellright && nx))
                 {
-                    zombieSprite.position.Y = game.TileToPixel(ty);
+                    turretSprite.position.Y = game.TileToPixel(ty);
                     this.velocity.Y = 0;
                     ny = false;
                 }
@@ -139,7 +127,7 @@ namespace ProjectCRYPT
             {
                 if ((cell && !celldown) || (cellright && !celldiag && nx))
                 {
-                    zombieSprite.position.Y = game.TileToPixel(ty + 1);
+                    turretSprite.position.Y = game.TileToPixel(ty + 1);
                     this.velocity.Y = 0;
                     cell = celldown;
                     cellright = celldiag;
@@ -151,30 +139,28 @@ namespace ProjectCRYPT
             {
                 if ((cellright && !cell) || (celldiag && !celldown && ny))
                 {
-                    zombieSprite.position.X = game.TileToPixel(tx);
+                    turretSprite.position.X = game.TileToPixel(tx);
                     this.velocity.X = 0;
-                    zombieSprite.Pause();
+                    turretSprite.Pause();
                 }
             }
             else if (this.velocity.X < 0)
             {
                 if ((cell && !cellright) || (celldown && !celldiag && ny))
                 {
-                    zombieSprite.position.X = game.TileToPixel(tx + 1);
+                    turretSprite.position.X = game.TileToPixel(tx + 1);
                     this.velocity.X = 0;
-                    zombieSprite.Pause();
+                    turretSprite.Pause();
                 }
-            }                   
+            }*/
 
         }
-        
-        
+
+
         public void Draw(SpriteBatch spriteBatch)
-        {          
-            zombieSprite.Draw(spriteBatch);
-            zombieAnimation.Rotation = zombieRotation;
-            //spriteBatch.DrawRectangle(zombieSprite.Bounds, Color.White, 1);
+        {
+            turretSprite.Draw(spriteBatch);
+            turretAnimation.Rotation = turretRotation;
         }
-                
     }
 }
